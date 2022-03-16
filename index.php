61baec6d052f2aa7587ba4a612ad3fbdb6b97d63
huymey <huymey.kum@student.passerellesnumeriques.org>
2022-03-16 08:03:07 +0700

    #17 design style html and css

--- index.php
+++ index.php
@@ -1,4 +1,55 @@
-<?php
-/**
- * Your code here
- */
+<?php require_once ('templates/header.php');?>
+<!-- navbar -->
+<nav>
+    <h3>facebook</h3>
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col post">
+            <div class="row-img">
+                <img src="images/rady.jpg" alt="">
+                <p>12 Mar 2022</p>
+            </div>
+            <div class="row-p">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur, ut. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore consequuntur vel voluptate earum soluta quis error provident veritatis harum laborum similique, recusandae adipisci. Ratione, perferendis culpa laborum eos aspernatur delectus!</p>
+            </div>
+            <div class="row-pic">
+                <img src="images/girls.jpg" alt="">
+            </div>
+            <div class="row-com-like">
+                <button class="like">like  23k</button>
+                <button class="comment">comment 50</button>
+            </div>
+        </div>
+        <div class="col">
+            <div class="relative">
+                <img src="images/rady.jpg" alt="">
+            </div>
+            <div class="upsolute">
+                <img src="images/rady.jpg" alt="">
+            </div>
+            <div class="name-profile">
+                <h3>MEY SOK</h3>
+            </div>
+            <div class="btn_post">
+            <button><a href="">What's on your mine</a></button>
+            </div>
+        </div>
+    </div>
+    
+</div>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require_once ('templates/footer.php');?>
