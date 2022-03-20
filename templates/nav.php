1c0baecd51bef8f174c512b882aa37d5972c8276
huymey <huymey.kum@student.passerellesnumeriques.org>
2022-03-20 15:30:03 +0700

    #11 add file in templates

---
+++ templates/nav.php
@@ -0,0 +1,8 @@
+<!--------------------------------navbar------------- -->
+
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Facebook</span>
+    <a href="#"><i class="fa fa fa-home" style="font-size:40px"></i></a>
+  </div>
+</nav>
