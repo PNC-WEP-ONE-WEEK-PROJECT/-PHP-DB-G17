041a1fc82fb225aab48bcdec9ab132d813684893
huymey <huymey.kum@student.passerellesnumeriques.org>
2022-03-20 19:27:16 +0700

    #3 add style

--- templates/body.php
+++ templates/body.php
@@ -8,34 +8,34 @@
     <?php
         $posts = get_post();
         foreach($posts as $post):
-            
+           
     ?>
-    
+
         <div class="card mt-5">
             <div class="card-header  w-25 h-75">
-            <img src="images/rady.jpg" class="rounded-circle w-50 h-75" alt="">
-            <p>2021-10-02</p>
+                <img src="images/rady.jpg" class="rounded-circle w-50 h-75" alt="">
+                <p>2021-10-02</p>
             </div>
             <div class="card-body">
                     <div class="p-2 ml-3"><?php echo $post['descriptoin'] ?></div> 
                     <div class="img-post w-100 "><img class="w-100" src="image_upload/<?= $post['imges']?>" alt=""></div>
-                </div>
+            </div>
 
-            <div class="container m-3">
+        <div class="container m-3">
 <!-- ---------------------------display like--------------------- -->
 
 <?php
     $likes=get_like($post['post_id']);
     foreach($likes as $like):
 ?>
-                 <form action="controllers/insert_like.php" method='post'class="d-flex mr-4">
-                     <button type='submit' class="">
-                         like ❤ <span><?= $like['num_of_like']?></span> 
+                <form action="controllers/insert_like.php" method='post'class="d-flex mr-4">
+                    <button type='submit' class="">
+                        like ❤ <span><?= $like['num_of_like']?></span> 
                     </button>
-                     <input type="hidden" name="like" style='display:none' value="<?php echo $post['post_id'] ?>">
-                 </form>
+                    <input type="hidden" name="like" style='display:none' value="<?php echo $post['post_id'] ?>">
+                </form>
 <?php endforeach ?>
-                 <button ><a href="views/display_comment_view.php?id=<?php echo $post['post_id'] ?>" >comment</a> </button>
+                <button ><a href="views/display_comment_view.php?id=<?php echo $post['post_id'] ?>" >comment</a> </button>
         </div>
 <!--------------------------- display comment-------------------- -->
             <?php
@@ -70,7 +70,7 @@
             <h3>MEY SOK</h3>
         </div>
         <div class="btn_post mt-3">
-            <button><a href="views/post_view.php">What's on your mine</a></button>
+            <button><a href="views/post_view.php">What's you mind</a></button> 
         </div>
-    </div>
+</div>
 </div>
--- templates/header.php
+++ templates/header.php
@@ -8,12 +8,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
     <!-- Your style here -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
     
