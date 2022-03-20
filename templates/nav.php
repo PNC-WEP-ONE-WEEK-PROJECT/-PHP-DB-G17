e89834b32637332e50c0fd1d802a72f80a8dbcc6
Chansok.OEUN <chansok.oeun@PNC.LAN>
2022-03-20 19:25:56 +0700

    #3 addtime

--- templates/nav.php
+++ templates/nav.php
@@ -3,6 +3,6 @@
 <nav class="navbar navbar-light bg-light">
   <div class="container-fluid">
     <span class="navbar-brand mb-0 h1">Facebook</span>
-    <a href="#"><i class="fa fa fa-home" style="font-size:40px"></i></a>
+    <a href="../index.php"><i class="fa fa fa-home" style="font-size:40px"></i></a>
   </div>
 </nav>
